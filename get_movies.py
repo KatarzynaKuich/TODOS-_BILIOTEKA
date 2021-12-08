81ad71fcb2ff7141e9c2dbe6ea76d50b021ac0d5
Katarzyna Kuich <89656991+KatarzynaKuich@users.noreply.github.com>
2021-12-08 13:51:05 +0100

    first commit

---
+++ get_movies.py
@@ -0,0 +1,22 @@
+import random
+
+from flask import Flask, render_template,redirect,url_for,request
+from models import todos
+from forms import TodoForm
+
+app = Flask(__name__)
+
+def get_movies(list_type='popular', how_many=8):
+    endpoint = f"https://api.themoviedb.org/3/movie/{list_type}"
+    response = requests.get(endpoint, headers=headers)
+    try:
+        response.raise_for_status()
+    except HTTPError:
+        get_movies()
+    data = response.json()
+    data = random.sample(data['results'], len(data['results']))
+    return data[:how_many]
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='TODOS _BILIOTEKA',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Neokatrina',
+    author_email='',
+    description=''
+)
