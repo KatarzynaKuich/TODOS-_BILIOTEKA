22c02607c5a9caf68917f11f463886492cc548f4
Katarzyna Kuich <89656991+KatarzynaKuich@users.noreply.github.com>
2021-12-12 12:37:57 +0100

    add multiple choise

--- forms.py
+++ forms.py
@@ -1,13 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,TextAreaField,URLField,IntegerField,SubmitField
-from wtforms.validators import DataRequired,NumberRange,Length
+from wtforms import StringField, TextAreaField, URLField, IntegerField, SubmitField, SelectMultipleField
+from wtforms.validators import DataRequired, NumberRange, Length
+
+
 class FilmForm(FlaskForm):
-    id =      IntegerField(validators=[DataRequired()])
-    title   = StringField(validators=[DataRequired()])
-    plot    = TextAreaField()
-    year    = StringField()
+    id = IntegerField(validators=[DataRequired()])
+    title = StringField(validators=[DataRequired()])
+    plot = TextAreaField()
+    year = StringField()
     actors = StringField()
-    posterUrl= StringField()
-    genres = StringField()
-
-
+    posterUrl = StringField()
+    genres = SelectMultipleField("Komedy", choices=["Comedy", "Drama", "Crime", "History", "War", "Thriller", "Music",
+                                                    "Adventure", "Sci-Fi","Romance","Action"], coerce=str, option_widget=None)
